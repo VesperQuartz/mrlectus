67ad59c6a1ddd568bc489691a5a805879b834acc
VesperQuartz <lilbrown3000@gmail.com>
2025-11-30 04:29:14 +0100

    small fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,17 @@
-import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
+import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import {
   createRootRouteWithContext,
   HeadContent,
   Outlet,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import type { QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
-import type { AuthClient } from "better-auth/react"; // Attempt to import AuthClient type
 import { AsyncProvider } from "@/providers/async";
 import { LoadingProvider } from "@/providers/loader";
 export type RootContext = {
-  auth: AuthClient; // Use the imported type
+  auth: any;
   queryClient: QueryClient | undefined;
 };
 
